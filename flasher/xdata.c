ee1f4dd07dcb5b7b7982e34c54472c0c84ee9f50
Khoa Hoang <admin@khoahoang.com>
2021-01-25 16:47:27 -0800

    Add VXIS xdata tools
    This tool allow read/write into VS2828 8051 xdata memory space

--- flasher/xdata.c
+++ flasher/xdata.c
@@ -26,83 +26,14 @@
 #include "log.h"
 #include "vxis.h"
 
-#define VS9989_I2C_SLAVE 0x88
-
 static int print_usage(const char *s)
 {
     fprintf(stderr, "Usage: %s (-r addr) (-w addr value) /dev/videoX\n\n", s);
-    fprintf(stderr, "Read register 0x11:\n");
-    fprintf(stderr, "    %s -r 0x11 /dev/video0\n", s);
-    fprintf(stderr, "Write 0x20 to register 0x11:\n");
-    fprintf(stderr, "    %s -w 0x11 0x20 /dev/video0\n", s);
+    fprintf(stderr, "Read register 0x2170:\n");
+    fprintf(stderr, "    %s -r 0x2170 /dev/video0\n", s);
+    fprintf(stderr, "Write 0x80 to register 0x2170:\n");
+    fprintf(stderr, "    %s -w 0x2170 0x20 /dev/video0\n", s);
     return EXIT_FAILURE;
-}
-
-static int vs9989_write_reg(uint8_t reg, uint8_t val)
-{
-    uint8_t status = 0;
-    int cnt;
-
-    //NOTE: will break if FW try to do i2c between these command
-    if (vxis_xdata_write(0x2600, VS9989_I2C_SLAVE)) goto err;
-    if (vxis_xdata_write(0x2601, 0x04)) goto err;
-    if (vxis_xdata_write(0x2603, 0x00)) goto err;
-    if (vxis_xdata_write(0x2608, 0x02)) goto err;
-    if (vxis_xdata_write(0x2610, reg)) goto err;
-    if (vxis_xdata_write(0x2611, val)) goto err;
-
-    cnt = 10;
-    while (cnt--) {
-        if (vxis_xdata_read(0x2650, &status)) goto err;
-        if (status == 0)
-            return 0;
-    }
-err:
-    return 1;
-}
-
-static int vs9989_read_reg(uint8_t reg, uint8_t *val)
-{
-    int cnt;
-    uint8_t status = 0;
-    
-    //NOTE: will break if FW try to do i2c between these command
-#if 0
-    /* This is to enable the i2c port. Seem to cause video to stop. Disable for now */
-    if (vxis_xdata_write(0x2004, 0x34)) goto err;
-    if (vxis_xdata_read(0x2790, &status)) goto err;
-    status &= ~(1<<6); /* clear bit 6 */
-    if (vxis_xdata_write(0x2790, 0xb1)) goto err;
-#endif
-
-    if (vxis_xdata_write(0x2600, VS9989_I2C_SLAVE)) goto err;
-    if (vxis_xdata_write(0x2601, 0x06)) goto err;
-    if (vxis_xdata_write(0x2603, 0x00)) goto err;
-    if (vxis_xdata_write(0x2608, 0x01)) goto err; //write len
-    if (vxis_xdata_write(0x2609, 0x01)) goto err; //read len
-    if (vxis_xdata_write(0x2610, reg)) goto err; //write data
-    if (vxis_xdata_write(0x2602, 0x13)) goto err;
-
-    cnt = 10;
-    while (cnt--) {
-        if (vxis_xdata_read(0x2650, &status)) goto err;
-        if (status == 0)
-            break;
-    }
-    if (!cnt)
-        goto err;
-
-    /* read value form i2c */
-    if (vxis_xdata_read(0x2610, &val[0])) goto err;
-
-    cnt = 10;
-    while (cnt--) {
-        if (vxis_xdata_read(0x2650, &status)) goto err;
-        if (status == 0)
-            return 0;
-    }
-err:
-    return 1;
 }
 
 int main (int argc, char *argv[])
@@ -110,7 +41,8 @@ int main (int argc, char *argv[])
     const char *devpath;
     int mode; /* 0 - read, 1 - write */
     int ret;
-    uint8_t addr, value;
+    uint16_t addr;
+    uint8_t value;
 
     if (argc < 4)
         return print_usage(argv[0]);
@@ -139,7 +71,7 @@ int main (int argc, char *argv[])
 
     switch (mode) {
     case 0: /* read */
-        ret = vs9989_read_reg(addr, &value);
+        ret = vxis_xdata_read(addr, &value);
         if (ret) {
             vxis_close();
             return EXIT_FAILURE;
@@ -147,7 +79,7 @@ int main (int argc, char *argv[])
         printf("%02x\n", value);
         break;
     case 1: /* write */
-        ret = vs9989_write_reg(addr, value);
+        ret = vxis_xdata_write(addr, value);
         if (ret) {
             vxis_close();
             return EXIT_FAILURE;
